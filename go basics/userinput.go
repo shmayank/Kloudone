30c3a1eb9064ca9b201ec3184c47fc4e6a25d26b
Monu Sharma <monu6597@gmail.com>
2020-11-18 14:28:24 +0530

    Add files via upload

---
+++ go basics/userinput.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+     var input int
+     var name string	 
+	 fmt.Println("Enter your input:")
+	 fmt.Scanln(&input)
+	 fmt.Scanln(&name)
+	 fmt.Println(input, name)
+	 
+
+}
